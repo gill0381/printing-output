3be3d7828ced2869d199e4e3008b80db48430842
gill0381 <gill0381@algonquinlive.com>
2017-05-15 22:26:23 -0400

    edited JavaScript

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Task 1
+// alert( "description of alerted_value: " + alerted_value );
+
+var a = 3;
+var b = 5;
+var c;
+
+alert("var a = 3; \nvar b = 5;\nvar c; \n_____________\n" + 
+      "a + b = " + (a + b) + "\n" +
+      "a - b = " + (a - b) + "\n" +
+      "a * b = " + (a * b) + "\n" +
+      "a / b = " + (a / b) + "\n" +
+      "a % b = " + (a % b) + "\n" +
+      "a += b = " + (a += b) + "\n" +
+      "a -= b = " + (a -= b) + "\n" +
+      "a *= b = " + (a *= b) + "\n" +
+      "a /= b = " + (a /= b) + "\n" +
+      "a %= b = " + (a %= b) + "\n" +
+      "a == b = " + (a == b) + "\n" +
+      "a != b = " + (a != b) + "\n" +
+      "a > b = " + (a > b) + "\n" +
+      "a < b = " + (a < b) + "\n" +
+      "!a && !c = " + (!a && !c) + "\n" +
+      "!a || !c = " + (!a || !c) + "\n");
+
+// Task 2
+var first_name = "Sukhmandeep";
+var last_name = "Gill";
+var email = "gill0381@algonquinlive.com";
+var output;
+
+output = "My name is" + " " + first_name + " " + last_name + ". " + " " + "You can contact me at" + " " + email + ". ";
+
+alert(output);
